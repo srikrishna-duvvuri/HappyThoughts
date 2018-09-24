576b3a8a72905ceef1673f049ff3032fd365621f
srikrishna-duvvuri <srikrishna.duvvuri1@gmail.com>
2018-09-23 21:13:40 -0400

    Worked on Login Activity

--- app/src/main/java/cs2340/happythoughts/WelcomeActivity.java
+++ app/src/main/java/cs2340/happythoughts/WelcomeActivity.java
@@ -1,13 +1,34 @@
 package cs2340.happythoughts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button mSignIn;
+    private Button mRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        mSignIn = findViewById(R.id.signInButton);
+        mRegistration = findViewById(R.id.registrationButton);
+
+        mSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+    }
+
+    private void goToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
