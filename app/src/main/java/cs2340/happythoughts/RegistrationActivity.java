a415ccd89d4bd65d85c60c28462710b2461b68f9
srikrishna-duvvuri <srikrishna.duvvuri1@gmail.com>
2018-09-30 20:39:11 -0400

    Changed spinner stuff

--- app/src/main/java/cs2340/happythoughts/RegistrationActivity.java
+++ app/src/main/java/cs2340/happythoughts/RegistrationActivity.java
@@ -61,7 +61,7 @@ public class RegistrationActivity extends AppCompatActivity implements LoaderCal
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
