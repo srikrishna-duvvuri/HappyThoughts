d399986334b3e018424ab27aa3cf5d3225321c7a
srikrishna-duvvuri <srikrishna.duvvuri1@gmail.com>
2018-10-18 15:28:06 -0400

    M6 stuff

---
+++ app/src/main/java/cs2340/happythoughts/Controllers/LocationListAdapter.java
@@ -0,0 +1,49 @@
+package cs2340.happythoughts.Controllers;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cs2340.happythoughts.Models.Location;
+import cs2340.happythoughts.R;
+
+public class LocationListAdapter extends ArrayAdapter<Location> {
+    private Context mContext;
+    private int mResource;
+    private TextView nameView;
+    private TextView addressView;
+
+    public LocationListAdapter(@NonNull Context context, int resource, ArrayList<Location> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        String name = getItem(position).getName();
+        String city = getItem(position).getCity();
+        String address = getItem(position).getStreetAddress();
+        String zip = getItem(position).getZip();
+        String state = getItem(position).getState();
+
+        String total = String.format("%s, %s, %s %s", address, city, state, zip);
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        convertView = inflater.inflate(mResource, parent, false);
+
+        nameView = (TextView) convertView.findViewById(R.id.locationName);
+        addressView = (TextView) convertView.findViewById(R.id.locationAddress);
+        nameView.setText(name);
+        addressView.setText(total);
+        return convertView;
+    }
+}
---
+++ app/src/main/java/cs2340/happythoughts/Models/Location.java
@@ -0,0 +1,129 @@
+package cs2340.happythoughts.Models;
+
+public class Location {
+    private int key;
+    private String name;
+    private double latitude;
+    private double longitude;
+    private String streetAddress;
+    private String city;
+    private String state;
+    private String zip;
+    private String type;
+    private String phone;
+    private String website;
+
+    public Location() {
+        //empty Location instance created
+    }
+
+    public Location(int key, String name, double latitude, double longitude,
+                    String streetAddress, String city, String state,
+                    String zip, String type, String phone,
+                    String website) {
+        this.key = key;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.type = type;
+        this.phone = phone;
+        this.website = website;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPhoneNumber() {
+        return phone;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phone = phone;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public String toString() {
+        return "Location Name: " + name;
+    }
+}
