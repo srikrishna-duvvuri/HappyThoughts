393188d2eb566a73bf95d2e05a551b115d556506
Mchakka <mchakka@gmail.com>
2018-10-28 22:57:32 -0400

    Added stuff

---
+++ app/src/main/java/cs2340/happythoughts/Controllers/AddDonationActivity.java
@@ -0,0 +1,15 @@
+package cs2340.happythoughts.Controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cs2340.happythoughts.R;
+
+public class AddDonationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_donation);
+    }
+}
