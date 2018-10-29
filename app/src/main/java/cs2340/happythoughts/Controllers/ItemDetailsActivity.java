9afbc81c89982aa8f712d8f5dc80747932d7dd83
Mchakka <mchakka@gmail.com>
2018-10-29 00:59:39 -0400

    Added stuff

--- app/src/main/java/cs2340/happythoughts/Controllers/ItemDetailsActivity.java
+++ app/src/main/java/cs2340/happythoughts/Controllers/ItemDetailsActivity.java
@@ -3,6 +3,8 @@ package cs2340.happythoughts.Controllers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cs2340.happythoughts.R;
+
 public class ItemDetailsActivity extends AppCompatActivity {
 
     @Override
