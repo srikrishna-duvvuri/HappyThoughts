29733e2c73e2aa33a40ebb7b8819c4899311f011
srikrishna-duvvuri <srikrishna.duvvuri1@gmail.com>
2018-10-29 00:44:54 -0400

    Added ItemDetailsActivity

---
+++ app/src/main/java/cs2340/happythoughts/Controllers/ItemDetailsActivity.java
@@ -0,0 +1,19 @@
+package cs2340.happythoughts.Controllers;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import cs2340.happythoughts.R;
+
+public class ItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+    }
+}
