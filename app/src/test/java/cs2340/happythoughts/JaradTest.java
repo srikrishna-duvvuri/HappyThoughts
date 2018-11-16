d1eed8d716263834df6246c320bbbd5df4e3297c
jaradhosking <jarad.hosking@gmail.com>
2018-11-16 16:59:39 -0500

    updated JaradTest

--- app/src/test/java/cs2340/happythoughts/JaradTest.java
+++ app/src/test/java/cs2340/happythoughts/JaradTest.java
@@ -1,8 +1,50 @@
 package cs2340.happythoughts;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import cs2340.happythoughts.Models.Location;
+import cs2340.happythoughts.Models.LocationsManager;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotSame;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(JUnit4.class)
 public class JaradTest {
+
+    Location loc1, loc2, loc3;
+    LocationsManager locMan = LocationsManager.getInstance();
+    @Before
+    public void initialize() {
+        loc1 = new Location(1,"one",33.7490,84.3880,"one way","onelanta","onegeorgia",
+                "1zip","1type","1phone","1.com");
+        loc2 = new Location(2,"two",39.8150,81.2260,"two way","twolanta","twogeorgia",
+                "2zip","2type","2phone","2.com");
+        loc3 = new Location(3,"three",44.2010,74.9200,"three way","threelanta","threegeorgia",
+                "3zip","3type","3phone","3.com");
+    }
+
+    @Test
+    public void checkLocationList() {
+        locMan.addLocation(loc1);
+        locMan.addLocation(loc2);
+        locMan.addLocation(loc3);
+        List<Location> locs = locMan.getLocations();
+        assertEquals(loc1, locs.get(0));
+        assertEquals(loc2, locs.get(1));
+        assertEquals(loc3, locs.get(2));
+    }
+
+    @Test
+    public void checkLocationListFailed() {
+        locMan.addLocation(loc1);
+        locMan.addLocation(loc2);
+        locMan.addLocation(loc2);
+        List<Location> locs = locMan.getLocations();
+        assertEquals(loc1, locs.get(0));
+        assertEquals(loc2, locs.get(1));
+        assertNotSame(loc3, locs.get(2));
+    }
 }
